3724a3054e9c5cbcb6eec71b8278a77cf6293151
Aung Heein zay <ahz007aunghz@gmail.com>
2025-10-12 00:31:42 +0630

    adding the nav

--- components/navigation/app-nav.tsx
+++ components/navigation/app-nav.tsx
@@ -2,15 +2,21 @@
 import { auth } from '@/server/auth'
 import NavLogo from './nav-logo'
 import UserButton from './user-button'
+import { CircleUserRound } from 'lucide-react'
+import Link from 'next/link'
 
 async function AppNav() {
     const session = await auth()
 
-    if(!session?.user)return null
   return (
     <header className='fixed w-full top-0 z-50 flex justify-between items-center h-16 px-4 py-2 bg-gray-100 shadow-2xl'>
         <NavLogo/>
-        <UserButton user={session.user} expires={session.expires}/>
+      {session?.user ? <UserButton user={session.user} expires={session.expires}/> : <Link href="auth/login" className="">
+  <CircleUserRound
+    className="text-gray-700" 
+    size={30} 
+  />
+</Link>}
     </header>
   )
 }
