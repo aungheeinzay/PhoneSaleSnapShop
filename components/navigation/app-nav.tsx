cb338eaf2974823e3a9be5fe75d3be9e528b680e
Aung Heein zay <ahz007aunghz@gmail.com>
2025-10-11 23:48:20 +0630

    i wish to be last

--- components/navigation/app-nav.tsx
+++ components/navigation/app-nav.tsx
@@ -1,16 +1,16 @@
-
+"use server"
 import { auth } from '@/server/auth'
 import NavLogo from './nav-logo'
 import UserButton from './user-button'
 
 async function AppNav() {
     const session = await auth()
-   
-    
+
+    if(!session?.user)return null
   return (
     <header className='fixed w-full top-0 z-50 flex justify-between items-center h-16 px-4 py-2 bg-gray-100 shadow-2xl'>
         <NavLogo/>
-        <UserButton user={session?.user!} expires={session?.expires!}/>
+        <UserButton user={session.user} expires={session.expires}/>
     </header>
   )
 }
